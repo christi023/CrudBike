94e83bcb125f91ed03e19c96d771acbfb2ca7740
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-02-20 12:33:11 +0100

    Create a ModelViewModel, model controller and added Model dropdown in layout.cshtml

---
+++ CrudBike/Controllers/ModelController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudBike.Models;
+using CrudBike.AppDBContext;
+using CrudBike.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudBike.Controllers
+{
+    public class ModelController : Controller
+    {
+        // dependency injection to access our Db Class
+        private readonly BikeDbContext _db;
+
+        [BindProperty]
+        public ModelViewModel ModelVM { get; set; }
+
+        public ModelController(BikeDbContext db)
+        {
+            _db = db;
+
+            ModelVM = new ModelViewModel()
+            {
+                Makes = _db.Makes.ToList(),
+                Model = new Models.Model()
+            };
+        }
+
+        public IActionResult Index()
+        {
+            var model = _db.Models.Include(m => m.Make);
+
+
+            return View(model);
+        }
+    }
+}
---
+++ CrudBike/Models/ViewModels/ModelViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CrudBike.Models.ViewModels
+{
+    public class ModelViewModel
+    {
+       public Model Model { get; set; }
+
+     //   public Model Make { get; set; }
+
+        public IEnumerable<Make> Makes { get; set; }
+    }
+}
